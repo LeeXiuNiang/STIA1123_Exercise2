7546899b09c7309a3cc288e3dbf2477027ef1c56
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 17:10:52 +0800

    Create Q1_Power.java

---
+++ Q1_Power.java
@@ -0,0 +1,18 @@
+public class Main {
+
+   public static void main(String[] args) {
+	 //Q1
+        String myFormat="%-8.0f%10.0f%10.0f\n";
+        System.out.printf("%-8s%10s%10s\n","Number","Square","Cube");
+        double[] number = new double[11];
+        double[] square = new double[11];
+        double[] cube = new double[11];
+
+        for(int i=0;i<11;i++){
+            number[i]=i;
+            square[i]=Math.pow(number[i],2);
+            cube[i]=Math.pow(number[i],3);
+            System.out.printf(myFormat,number[i],square[i],cube[i]);
+        }
+    }
+}
