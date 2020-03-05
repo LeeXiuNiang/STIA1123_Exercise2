254b46da7cb36f7b9c6b8f696af6d04aac3b0044
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 17:18:16 +0800

    Create Q2_CountA.java

---
+++ Q2_CountA.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountA {
+
+    public static void main(String[] args) {
+        //Q2
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = input.nextLine();
+        int count=0;
+        for(int i=0; i<str.length();i++){
+            if(str.charAt(i)=='A'||str.charAt(i)=='a'){
+                count++;
+            }
+        }
+        System.out.println("number of character 'A' found: "+ count);
+
+    }
+}
