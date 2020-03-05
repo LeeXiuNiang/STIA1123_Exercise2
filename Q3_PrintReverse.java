e8d1beaf5b65b837685aa2db3068ee3710f7fd2a
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 17:19:02 +0800

    Create Q3_PrintReverse.java

---
+++ Q3_PrintReverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PrintReverse {
+
+    public static void main(String[] args) {
+        //Q3
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str2 = input.nextLine();
+        System.out.print("In reverse order: ");
+        for(int i=str2.length()-1; i>=0; i--){
+            System.out.print(str2.charAt(i));
+        }
+    }
+}
